38823dbadc57fb210688017302bf8ed325c82cd1
asus-pc <asus-pc@asus>
2014-12-22 18:58:10 +0100

    Vista de cada coche

--- application/controllers/articulos.php
+++ application/controllers/articulos.php
@@ -39,6 +39,28 @@ class Articulos extends CI_Controller {
       //cargo la vista pasando los datos de configuacion
       $this->load->view('home', $datos_vista);
    }
+   
+   function muestra($id){
+      //cargo el helper de url, con funciones para trabajo con URL del sitio
+      $this->load->helper('url');
+      
+      //cargo el modelo de artículos
+      $this->load->model('Coche_model');
+      
+      //pido al modelo el artículo que se desea ver
+      $arrayArticulo = $this->Coche_model->dame_coche($id);
+      
+      //compruebo si he recibido un artículo
+      if (!$arrayArticulo){
+         //no he recibido ningún artículo
+         //voy a lanzar un error 404 de página no encontrada
+         show_404();
+      }else{
+         //he encontrado el artículo
+         //muestro la vista de la página de mostrar un artículo pasando los datos del array del artículo
+         $this->load->view('muestraCoche', $arrayArticulo);
+      }
+   }
     
 
     function ordenadores() {
--- application/models/coche_model.php
+++ application/models/coche_model.php
@@ -10,5 +10,16 @@ class Coche_model extends CI_Model {
         $ssql = "select * from coche";
         return mysql_query($ssql);
     }
+    
+    //nos devuleve un array con los coches
+    function dame_coche($id){
+      $ssql = "select * from coche where id=" . $id;
+      $rs = mysql_query($ssql);
+      if (mysql_num_rows($rs)==0){
+         return false;
+      }else{
+         return mysql_fetch_array($rs);
+      }
+   }
 
 }
