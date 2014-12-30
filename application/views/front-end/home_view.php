c017f67be7a32a44b646fad3309622d68574e231
asus-pc <asus-pc@asus>
2014-12-30 13:17:41 +0100

    Portada - modal - Listo para crear formulario

--- application/views/front-end/home_view.php
+++ application/views/front-end/home_view.php
@@ -6,10 +6,40 @@
         <link href="<?php echo base_url(); ?>assets/css/estilos.css" rel="stylesheet" type="text/css">
     </head>
     <body>
+
+        <section class="row portada-inicio">
+            <div class="col s8">
+                <h1>PARKEASY</h1>
+            </div>            
+        </section>
+        
+        <section class="row contenedor-botones-inicio">
+            <div class="col s8 m6 l4 offset-s2 offset-m3 offset-l4">
+                <div class="row">
+                    <div class="col s8 m6 l4 offset-s2 offset-m3 offset-l4">
+                        <a class="waves-effect waves-light btn-large inicio modal-activar" href="#modal_entrar">Entrar</a>
+                        <a class="waves-effect waves-light btn-large inicio">Registrarse</a>
+                        <!-- Modal Structure -->
+                        <div id="modal_entrar" class="modal">
+                            <form>
+                                
+                            </form>
+                            <a href="#" class="waves-effect btn-flat modal-close">Entrar</a>
+                        </div>
+
+                    </div>    
+                </div>
+            </div>
+        </section>    
+
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <script type="text/javascript"  src="<?php echo base_url(); ?>assets/js/materialize.js"></script>
-        
-        
-        
+        <script type="text/javascript"  src="<?php echo base_url(); ?>assets/js/materialize.js"></script> 
+        <script>
+
+            $(document).ready(function() {
+                $('.modal-activar').leanModal();
+            });
+
+        </script>
     </body>
 </html>    
